a05571a2ed86b6a0d3f2d920b822fce1ec9d7c9c
Bjørnar Prytz <bjornarprytz@gmail.com>
2025-10-12 19:53:56 +0200

    try latest godot version

--- src/logic/effect.gd
+++ src/logic/effect.gd
@@ -1,15 +1,16 @@
+@abstract
 class_name Effect
 extends Resource
 
 func validate(hit_info: HitInfo, action: ActionHandle) -> bool:
-	if hit_info.target and action.source:
-		if hit_info.target != action.source:
-			return true
-		push_warning("Effect cannot target the source character itself.")
-		return false
+    if hit_info.target and action.source:
+        if hit_info.target != action.source:
+            return true
+        push_warning("Effect cannot target the source character itself.")
+        return false
 
-	push_warning("Effect validation failed: Missing target or source.")
-	return false
+    push_warning("Effect validation failed: Missing target or source.")
+    return false
 
-func resolve(hit_info: HitInfo, action: ActionHandle) -> void:
-	pass
+@abstract
+func resolve(hit_info: HitInfo, action: ActionHandle) -> void
